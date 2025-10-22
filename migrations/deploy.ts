2538f25765be38fa6db41c8ceac7536dde5b35f4
xxpopielxx <kammilkaczmarczyk@gmail.com>
2025-10-22 22:39:59 +0200

    readme

--- migrations/deploy.ts
+++ migrations/deploy.ts
@@ -5,8 +5,8 @@
 import * as anchor from "@coral-xyz/anchor";
 
 module.exports = async function (provider: anchor.AnchorProvider) {
-  // Configure client to use the provider.
+  // Need to configure client to use the provider.
   anchor.setProvider(provider);
 
-  // Add your deploy script here.
+  //script here.
 };
--- tests/yield-flow.ts
+++ tests/yield-flow.ts
@@ -3,13 +3,13 @@ import { Program } from "@coral-xyz/anchor";
 import { YieldFlow } from "../target/types/yield_flow";
 
 describe("yield-flow", () => {
-  // Configure the client to use the local cluster.
+  // Need to configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.yieldFlow as Program<YieldFlow>;
 
   it("Is initialized!", async () => {
-    // Add your test here.
+    // Test here.
     const tx = await program.methods.initialize().rpc();
     console.log("Your transaction signature", tx);
   });
